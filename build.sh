664ef21882976b764911b96d1d227ef7f8fa0405
Johan Hernefeldt <johan.hernefeldt@teliacompany.com>
2020-06-09 17:36:59 +0200

    Removed duplicate tagging

--- build.sh
+++ build.sh
@@ -10,6 +10,5 @@ IMAGE_VERSION=1.0.0
 DOCKER_BUILDKIT=1 docker image build -t ${IMAGE_NAME}:${IMAGE_VERSION} .
 
 docker image tag ${IMAGE_NAME}:${IMAGE_VERSION} ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}
-docker image tag ${IMAGE_NAME}:${IMAGE_VERSION} ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}
 
 docker image ls | grep ${IMAGE_NAME} | grep $IMAGE_VERSION
